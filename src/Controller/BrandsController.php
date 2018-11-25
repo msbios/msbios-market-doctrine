eb248579617941f1ffd28906072cdcb65e36ec59
Judzhin Miles <info@msbios.com>
2018-11-25 22:23:52 +0200

    Lazy Composer Commiter

--- src/Controller/BrandsController.php
+++ src/Controller/BrandsController.php
@@ -7,8 +7,12 @@
 namespace MSBios\Market\Doctrine\Controller;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\QueryBuilder;
 use MSBios\Market\Doctrine\Mvc\AbstractActionController;
 use MSBios\Market\Resource\Doctrine\Entity\Brand;
+use MSBios\Market\Resource\Doctrine\Entity\Product;
+use MSBios\Paginator\Doctrine\Adapter\QueryBuilderPaginator;
+use Zend\Paginator\Paginator;
 use Zend\View\Model\ModelInterface;
 
 /**
@@ -34,10 +38,32 @@ class BrandsController extends AbstractActionController
             return $this->notFoundAction();
         }
 
+        /**
+         * @param QueryBuilder $qb
+         * @return QueryBuilderPaginator
+         */
+        $fetchBy = function (QueryBuilder $qb) use ($brand) {
+            $qb->where($qb->expr()->eq('o.brand', ':brand'))
+                ->andWhere($qb->expr()->eq('o.rowStatus', true))
+                ->setParameter('brand', $brand);
+
+            return new QueryBuilderPaginator($qb);
+        };
+
+        /** @var Paginator $products */
+        $products = $dem->getRepository(Product::class)
+            ->fetchAll($fetchBy);
+
+        $products
+            ->setPageRange(ProductsController::PAGE_RANGE)
+            ->setItemCountPerPage(ProductsController::ITEM_COUNT_PER_PAGE)
+            ->setCurrentPageNumber($this->params()->fromQuery('page', 1));
+
         /** @var ModelInterface $viewModel */
         $viewModel = parent::indexAction();
         $viewModel->setVariables([
             'brand' => $brand,
+            'products' => $products,
         ]);
 
         return $viewModel;
